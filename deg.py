bdc4cb1b50298ac246d82d08b12b3e6cd27bf902
james-r-barrett <118378540+james-r-barrett@users.noreply.github.com>
2024-05-08 18:23:22 +0100

    intial commit

---
+++ deg.py
@@ -0,0 +1,7 @@
+# script to import quant data from salmon mapped DEG
+# files to import are tsv files in the format as below
+#Name	Length	EffectiveLength	TPM	NumReads
+#jgi|Chloso1230_1_1|1|CSI2_123000007745-RA	1254	1004.000	27.104241	2500.106
+
+# we need the first column (Name) and the fourth column (TPM) to
+
